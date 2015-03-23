f8ca8453e6c7adfc90736693a40c5e5837bfef38
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2015-03-23 09:14:34 +0100

    Configurable branch deletion for the seed branching strategy

--- src/test/java/net/nemerosa/seed/jenkins/strategy/seed/SeedBranchStrategyTest.java
+++ src/test/java/net/nemerosa/seed/jenkins/strategy/seed/SeedBranchStrategyTest.java
@@ -1,5 +1,6 @@
 package net.nemerosa.seed.jenkins.strategy.seed;
 
+import com.google.common.collect.ImmutableMap;
 import net.nemerosa.seed.jenkins.Constants;
 import net.nemerosa.seed.jenkins.SeedLauncher;
 import net.nemerosa.seed.jenkins.model.SeedConfiguration;
@@ -35,6 +36,71 @@ public class SeedBranchStrategyTest {
                 Constants.BRANCH_PARAMETER,
                 "master"
         ));
+    }
+
+    @Test
+    public void post_delete_default() {
+        SeedBranchStrategy strategy = new SeedBranchStrategy();
+
+        SeedLauncher launcher = mock(SeedLauncher.class);
+
+        strategy.post(
+                new SeedEvent(
+                        "nemerosa/ontrack",
+                        "feature/xxx",
+                        SeedEventType.DELETION
+                ),
+                launcher,
+                new SeedConfiguration(Collections.<String, Object>emptyMap()),
+                SeedProjectConfiguration.of("nemerosa/ontrack")
+        );
+
+        verify(launcher, times(1)).delete("ontrack/ontrack-feature-xxx");
+    }
+
+    @Test
+    public void post_delete_pipeline_not_deleted_global_config() {
+        SeedBranchStrategy strategy = new SeedBranchStrategy();
+
+        SeedLauncher launcher = mock(SeedLauncher.class);
+
+        strategy.post(
+                new SeedEvent(
+                        "nemerosa/ontrack",
+                        "feature/xxx",
+                        SeedEventType.DELETION
+                ),
+                launcher,
+                new SeedConfiguration(Collections.singletonMap("pipeline-delete", "no")),
+                SeedProjectConfiguration.of("nemerosa/ontrack")
+        );
+
+        verify(launcher, times(1)).delete("ontrack/ontrack-feature-xxx/ontrack-feature-xxx-seed");
+    }
+
+    @Test
+    public void post_delete_pipeline_not_deleted_project_config() {
+        SeedBranchStrategy strategy = new SeedBranchStrategy();
+
+        SeedLauncher launcher = mock(SeedLauncher.class);
+
+        strategy.post(
+                new SeedEvent(
+                        "nemerosa/ontrack",
+                        "feature/xxx",
+                        SeedEventType.DELETION
+                ),
+                launcher,
+                new SeedConfiguration(Collections.singletonMap("pipeline-delete", "yes")),
+                SeedProjectConfiguration.of(
+                        ImmutableMap.of(
+                                "id", "nemerosa/ontrack",
+                                "pipeline-delete", "no"
+                        )
+                )
+        );
+
+        verify(launcher, times(1)).delete("ontrack/ontrack-feature-xxx/ontrack-feature-xxx-seed");
     }
 
 }
