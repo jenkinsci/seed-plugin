33f096e2765a611373508b74fee4e2cd3e05efd6
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2015-04-21 09:17:43 +0200

    Docker files

---
+++ src/main/docker/seed-job-init.groovy
@@ -0,0 +1,28 @@
+import hudson.model.*
+import jenkins.model.*
+
+Thread.start {
+    sleep 10000
+    println "--> generating the seed job"
+    def process = new ProcessBuilder(
+            'java',
+            '-jar',
+            '/var/lib/jenkins/init.groovy.d/job-dsl-core.jar',
+            '/var/lib/jenkins/dsl/seed-job-dsl.groovy'
+    )
+            .directory(new File('/var/lib/jenkins/dsl'))
+            .redirectErrorStream(true)
+            .start()
+    int code = process.waitFor()
+    if (code != 0) {
+        def message = "Error while generating a job: [${code}]"
+        println message
+        println process.text
+        throw new RuntimeException(message)
+    }
+    // Importing the XML as a new job
+    println "--> creating the seed job"
+    new File('/var/lib/jenkins/dsl/seed.xml').withInputStream {
+        Jenkins.instance.createProjectFromXML('seed', it)
+    }
+}
