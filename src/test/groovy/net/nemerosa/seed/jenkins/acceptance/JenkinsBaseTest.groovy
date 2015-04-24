b69877f4c8361787b441e903da634b68dbb0fd36
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2015-04-24 17:19:31 +0200

    Test for the branch seed creation

--- src/test/groovy/net/nemerosa/seed/jenkins/acceptance/JenkinsBaseTest.groovy
+++ src/test/groovy/net/nemerosa/seed/jenkins/acceptance/JenkinsBaseTest.groovy
@@ -38,10 +38,21 @@ class JenkinsBaseTest {
         ]).checkSuccess()
         // Checks the project seed is created
         jenkins.job('test/test-seed')
-        // TODO Fires the project seed
+        // Fires the project seed
+        jenkins.fireJob('test/test-seed', [
+                BRANCH: 'master'
+        ]).checkSuccess()
+        // Checks the branch seed is created
+        jenkins.job('test/test-master/test-master-seed')
         // TODO Fires the branch seed
+        // jenkins.fireJob('test/test-master/test-master-seed').checkSuccess()
+        // TODO Checks the branch pipeline is there
+        // jenkins.job('test/test-master/test-master-build')
+        // jenkins.job('test/test-master/test-master-ci')
+        // jenkins.job('test/test-master/test-master-publish')
         // TODO Fires the branch pipeline start
-        // TODO Checks the result of the pipeline
+        // jenkins.fireJob('test/test-master/test-master-build').checkSuccess()
+        // TODO Checks the result of the pipeline (ci & publish must have been fired)
     }
 
 }
