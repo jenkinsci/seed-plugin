0193380e9388e1ef35a8b4bd78d2b5bd32cfda20
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2015-04-26 21:15:17 +0200

    End point to update the seed configuration programmatically

---
+++ src/main/groovy/net/nemerosa/seed/jenkins/config/SeedConfigurationEndPoint.java
@@ -0,0 +1,46 @@
+package net.nemerosa.seed.jenkins.config;
+
+import hudson.Extension;
+import hudson.model.RootAction;
+import jenkins.model.GlobalConfiguration;
+import net.nemerosa.seed.jenkins.SeedPlugin;
+import net.nemerosa.seed.jenkins.model.SeedConfiguration;
+import org.apache.commons.io.IOUtils;
+import org.kohsuke.stapler.StaplerRequest;
+import org.kohsuke.stapler.StaplerResponse;
+import org.kohsuke.stapler.interceptor.RequirePOST;
+
+import java.io.IOException;
+
+@Extension
+public class SeedConfigurationEndPoint implements RootAction {
+
+    @RequirePOST
+    public void doIndex(StaplerRequest req, StaplerResponse rsp) throws IOException {
+        // Gets the payload
+        String payload = IOUtils.toString(req.getReader());
+        // Parsing for test
+        SeedConfiguration.parseYaml(payload);
+        // If OK, going on with the configuration
+        SeedPlugin seedPlugin = GlobalConfiguration.all().get(SeedPlugin.class);
+        seedPlugin.setSeedConfigurationContent(payload);
+        seedPlugin.save();
+        // OK
+        System.out.println("Seed configuration updated.");
+    }
+
+    @Override
+    public String getIconFileName() {
+        return null;
+    }
+
+    @Override
+    public String getDisplayName() {
+        return null;
+    }
+
+    @Override
+    public String getUrlName() {
+        return "seed-config";
+    }
+}
