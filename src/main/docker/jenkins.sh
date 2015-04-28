5db57cdd34deb8612c18fa1b5796de3ad74f3119
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2015-04-28 18:27:44 +0200

    More traces about the plugin copy

--- src/main/docker/jenkins.sh
+++ src/main/docker/jenkins.sh
@@ -1,17 +1,22 @@
 #! /bin/bash
 
 # Copy of plugins
+echo "Copy of plugins in ${JENKINS_HOME}/plugins"
 mkdir -p ${JENKINS_HOME}/plugins
-find /usr/share/jenkins/ref/plugins -type f -exec cp -f {} ${JENKINS_HOME}/plugins \;
+rm -rf ${JENKINS_HOME}/plugins/*
+find /usr/share/jenkins/ref/plugins -type f -exec cp -fv {} ${JENKINS_HOME}/plugins \;
 
 # Copy of DSL scripts
+echo "Copy of DSL scripts in ${JENKINS_HOME}/dsl"
 mkdir -p ${JENKINS_HOME}/dsl
-find /usr/share/jenkins/ref/dsl -type f -exec cp -f {} ${JENKINS_HOME}/dsl \;
+rm -rf ${JENKINS_HOME}/dsl/*
+find /usr/share/jenkins/ref/dsl -type f -exec cp -fv {} ${JENKINS_HOME}/dsl \;
 
 # Copy of initialisation scripts
+echo "Copy of initialisation scripts in ${JENKINS_HOME}/init.groovy.d"
 rm -rf ${JENKINS_HOME}/init.groovy.d
 mkdir -p ${JENKINS_HOME}/init.groovy.d
-find /usr/share/jenkins/ref/init.groovy.d -type f -exec cp -f {} ${JENKINS_HOME}/init.groovy.d \;
+find /usr/share/jenkins/ref/init.groovy.d -type f -exec cp -fv {} ${JENKINS_HOME}/init.groovy.d \;
 
 # if `docker run` first argument start with `--` the user is passing jenkins launcher arguments
 if [[ $# -lt 1 ]] || [[ "$1" == "--"* ]]; then
