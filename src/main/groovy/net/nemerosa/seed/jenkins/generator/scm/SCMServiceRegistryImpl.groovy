cae77476980aac5dd72e56c58e504efe3a158568
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2015-04-28 17:59:24 +0200

    SVN SCM service

--- src/main/groovy/net/nemerosa/seed/jenkins/generator/scm/SCMServiceRegistryImpl.groovy
+++ src/main/groovy/net/nemerosa/seed/jenkins/generator/scm/SCMServiceRegistryImpl.groovy
@@ -4,11 +4,14 @@ import net.nemerosa.seed.jenkins.generator.scm.SCMService
 import net.nemerosa.seed.jenkins.generator.scm.SCMServiceNotDefinedException
 import net.nemerosa.seed.jenkins.generator.scm.SCMServiceRegistry
 import net.nemerosa.seed.jenkins.generator.scm.git.GitSCMService
+import net.nemerosa.seed.jenkins.generator.scm.svn.SvnSCMService
 
 class SCMServiceRegistryImpl implements SCMServiceRegistry {
 
+    // TODO Injection
     private final Map<String, SCMService> scmServices = [
-            git: new GitSCMService()
+            git: new GitSCMService(),
+            svn: new SvnSCMService(),
     ]
 
     @Override
---
+++ src/main/groovy/net/nemerosa/seed/jenkins/generator/scm/svn/SvnSCMService.groovy
@@ -0,0 +1,27 @@
+package net.nemerosa.seed.jenkins.generator.scm.svn
+
+import net.nemerosa.seed.jenkins.generator.scm.SCMService
+import net.nemerosa.seed.jenkins.support.SeedProjectEnvironment
+
+class SvnSCMService implements SCMService {
+
+    @Override
+    String getId() {
+        'svn'
+    }
+
+    @Override
+    String generatePartial(SeedProjectEnvironment env, String branch, String path) {
+        String credentialsId = env.getConfigurationValue(SCM_CREDENTIALS_ID, '')
+        """\
+scm {
+    svn {
+        location('${env.scmUrl}/${path}') {
+            directory '${path}'
+            credentials '${credentialsId}'
+        }
+    }
+}
+"""
+    }
+}
