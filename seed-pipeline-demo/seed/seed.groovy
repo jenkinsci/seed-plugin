d49ff8f3d2a2f3718e9eb4549432fac73a5a8449
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2015-04-29 20:33:31 +0200

    Seed pipeline demo module

---
+++ seed-pipeline-demo/seed/seed.groovy
@@ -0,0 +1,13 @@
+/**
+ * Script being called to generate a pipeline
+ *
+ * The Seed plug-in will give the following parameters to this scripts, available directly as variables:
+ *
+ *
+ * The jobs are generated directly at the level of the branch seed job, so no folder needs to be created for the
+ * branch itself.
+ */
+
+freeStyleJob('test') {
+
+}
