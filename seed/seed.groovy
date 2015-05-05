cd21174e50568843572158fd01affb62c8008be1
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2015-05-05 10:00:17 +0200

    Seed script

---
+++ seed/seed.groovy
@@ -0,0 +1,45 @@
+/**
+ * Pipeline generation for the Seed plug-in.
+ *
+ * The Seed plug-in will give the following parameters to this scripts, available directly as variables:
+ *
+ * - raw parameters (seed generator input + scm branch)
+ *   - PROJECT - raw project name, like nemerosa/seed in GitHub
+ *   - PROJECT_CLASS
+ *   - PROJECT_SCM_TYPE
+ *   - PROJECT_SCM_URL
+ *   - BRANCH - basic branch name in the SCM, like branches/xxx in SVN
+ *
+ * - computed parameters:
+ *   - SEED_PROJECT: project normalised name
+ *   - SEED_BRANCH: branch normalised name
+ *
+ * The jobs are generated directly at the level of the branch seed job, so no folder needs to be created for the
+ * branch itself.
+ */
+
+freeStyleJob("${SEED_PROJECT}-${SEED_BRANCH}-build") {
+    logRotator(-1, 40)
+    jdk 'JDK7'
+    scm {
+        git {
+            remote {
+                url "git@github.com:nemerosa/seed-plugin.git"
+                branch "origin/${BRANCH}"
+            }
+            wipeOutWorkspace()
+            localBranch "${BRANCH}"
+        }
+    }
+    steps {
+        gradle 'clean localDockerAcceptanceTest --info --profile'
+    }
+    publishers {
+        archiveJunit("**/build/test-results/*.xml")
+        tasks(
+                '**/*.java,**/*.groovy,**/*.xml,**/*.html,**/*.js',
+                '**/build/**',
+                'FIXME', 'TODO', '@Deprecated', true
+        )
+    }
+}
