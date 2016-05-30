adf6988a825e4f8c085fb22f3f7ae6545566f0e1
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2016-05-30 18:26:05 +0200

    (v1) Pipeline library - correction of tests

--- seed-plugin/src/test/groovy/net/nemerosa/jenkins/seed/integration/GenerationIntegrationTest.groovy
+++ seed-plugin/src/test/groovy/net/nemerosa/jenkins/seed/integration/GenerationIntegrationTest.groovy
@@ -3,7 +3,6 @@ package net.nemerosa.jenkins.seed.integration
 import net.nemerosa.jenkins.seed.config.NamingStrategyConfig
 import net.nemerosa.jenkins.seed.config.PipelineConfig
 import net.nemerosa.jenkins.seed.integration.git.GitRepo
-import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 
@@ -452,7 +451,7 @@ class GenerationIntegrationTest {
         jenkins.checkJobExists("${projectName}/${projectName}_MASTER/${projectName}_MASTER_030_PUBLISH")
         // Checks the result of the pipeline (build, ci & publish must have been fired)
         jenkins.getBuild("${projectName}/${projectName}_MASTER/${projectName}_MASTER_010_BUILD", 1).checkSuccess()
-        jenkins.getBuild("${projectName}/${projectName}_MASTER/${projectName}_MASTER_030_PUBLISH", 1).checkSuccess()
+        jenkins.getBuild("${projectName}/${projectName}_MASTER/${projectName}_MASTER_020_CI", 1).checkSuccess()
         jenkins.getBuild("${projectName}/${projectName}_MASTER/${projectName}_MASTER_030_PUBLISH", 1).checkSuccess()
     }
 
@@ -501,7 +500,7 @@ class GenerationIntegrationTest {
         // Creates a Git repository with a seed.properties file
         String userDir = System.getProperty('user.dir')
         String seedProperties = """\
-seed.dsl.repository = flat:${userDir}/seed-plugin/build/integration/repository
+seed.dsl.repository = flat:${userDir}/build/integration/repository
 seed.dsl.libraries = :seed-pipeline-demo:+
 seed.dsl.script.jar = seed-pipeline-demo
 """
@@ -547,7 +546,7 @@ seed.dsl.script.jar = seed-pipeline-demo
         jenkins.fireJob("${projectName}/${projectName}_MASTER/${projectName}_MASTER_010_BUILD")
         // Checks the result of the pipeline (build, ci & publish must have been fired)
         jenkins.getBuild("${projectName}/${projectName}_MASTER/${projectName}_MASTER_010_BUILD", 1).checkSuccess()
-        jenkins.getBuild("${projectName}/${projectName}_MASTER/${projectName}_MASTER_030_PUBLISH", 1).checkSuccess()
+        jenkins.getBuild("${projectName}/${projectName}_MASTER/${projectName}_MASTER_020_CI", 1).checkSuccess()
         jenkins.getBuild("${projectName}/${projectName}_MASTER/${projectName}_MASTER_030_PUBLISH", 1).checkSuccess()
     }
 }
